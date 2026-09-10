609d02dd81776022d36b0b232a61aa8d8c91aca9
Douglas Hanley <thesecretaryofwar@gmail.com>
2026-09-10 13:55:45 -0500

    fix sympoly

--- docs/code/SymPoly.jsx
+++ docs/code/SymPoly.jsx
@@ -1,6 +1,6 @@
 // Draw a rounded star shape with a blue fill. Wrap it in a rounded frame.
 const rad = t => 1 - 0.2 * cos(5 * (t - pi/2))
-return <Frame rounded padding margin>
+return <Frame rounded padding={2} margin>
   <SymPoly aspect fill={blue}
     tlim={[0, 2*pi]} N={200}
     f={t => polar(t, rad(t))}
